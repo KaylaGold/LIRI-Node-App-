50439ffd9d9ecbe6d35cd5fa0b5e608f54bbcbb4
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-07-18 18:58:01 -0700

    commit 3

--- liri.js
+++ liri.js
@@ -1,7 +1,5 @@
 // Code to read and set any environment variables with the dotenv package
-require("dotenv").config();
-
-
+const dotenv = require("dotenv").config();
 
 // Request npm package
 let Twitter = require('twitter');
@@ -17,54 +15,55 @@ let client = new Twitter(keys.twitter);
 let omdb = keys.omdb.api_key;
 
 // Arguments 
-// First Argument: commands eg 'my-tweets', 'spotify-this-song'etc
-let command = process.argv[2]; 
-// Second Argument: when user gives us input next to the command in the command line eg node liri.js spotify-this-song "bendita tu luz"
-let input = process.argv[3]; 
+// First Argument: Commands eg 'my-tweets', 'spotify-this-song'etc
+let command = process.argv[2];
+// Second Argument: When user gives us input next to the command in the command line eg node liri.js spotify-this-song "bendita tu luz". The "bendita tu luz" is user input
+let input = process.argv[3];
 
 
-// Conditional statements for commands. This is app functionality due to user input
+// Conditional statements for arguments (commands). This is app functionality due to user input
 switch (command) {
     case "my-tweets":
+        // No user input necessary here
         myTweets();
         break;
 
     case "spotify-this-song":
-        // If there is an input
+        // If there is an input from user
         if (input) {
             spotifyThisSong(input);
         } else {
-            // if there is no input
-            spotifyThisSong("The Sign");
+            // If there is no input from user
+            spotifyThisSong('The Sign');
         }
         break;
 
     case "movie-this":
+        // If there is an input from user
         if (input) {
             movieThis(input);
         } else {
-            movieThis("Mr. Nobody");
+            // If there is no input from user
+            movieThis('Mr. Nobody');
         }
         break;
 
     case "do-what-it-says":
-        if (input) {
-            doWhatItSays(input);
-        } else {
-            doWhatItSays("I Want it That Way");
-        }
+        // No user input necessary here
+        doWhatItSays();
         break;
     default:
-        console.log('Invalid command. Please try again Lady!');
+        console.log('Hello, invalid command. Please try again!');
 };
 
 
-
-
-// Functions for 3 main functions of the app
+// Functions for the arguments (commands) 
+// Command #1 shows the last 20 tweets and when they were created
 function myTweets() {
 
-    let screenName = { screen_name: '@LagLiri' };
+    let screenName = {
+        screen_name: '@LagLiri'
+    };
     client.get('statuses/user_timeline', screenName, function (error, tweets, response) {
         if (!error) {
             console.log(tweets);
@@ -85,25 +84,71 @@ function myTweets() {
     });
 
 };
-
-// Command #2 display the artist, song name, preview link of teh song from Spotify, the album 
+// Command #2 display the artist, song name, preview link of the song from Spotify, the album 
 //that the song is from, and if no song is provided then default to "The Sign" by Ace of Base
-
-function spotifyThisSong(trackQuery) {
-    spotify.search({ type: 'track', query: trackQuery, limit: 1 }, function (err, data) {
+function spotifyThisSong(song) {
+    spotify.search({ type: 'track', query: song, limit: 1}, function (err, data) {
 
         if (err) {
             return console.log('Error occurred: ' + err);
-        } else{
-            for(let i=0; i<data.tracks.items.length; i++);
-
-        }
+      } else {
+        for (let i = 0; i < data.tracks.items.length; i++);{
+             let spotifySongData = data.tracks.items[i];
+             //Display artist
+         console.log("Artist:" + spotifySongData.artists.name);
+         console.log("Songs name:" + spotifySongData.name);
+         console.log("Preview link of the song is:"+ spotifySongData.preview_url);
+         console.log("Album name is:" + spotifySongData.album.name);
+         console.log("-----------------------");
+         }
+         
+     }
 
         console.log(data.tracks.items[0]);
     });
-   
+
 }
 
+// Command #3 OMDB will display title of movie, year it came out, IMDB rating, Rotten Tomatoes rating,
+//country where movie was produced, language of movie, plot, and actors in the movie
+function  movieThis(movie){
+    let queryUrl = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + omdb + "&plot=short&tomatoes=true";
+  
+    request(queryUrl, function(error, response, body){
+        //If request is successful
+        if(!error && response.statusCode === 200) {
+            console.log(body); //remove when done
+            console.log("Title of the movie is:"+ JSON.parse(body).Title);
+            console.log("Year the movie came out is:" + JSON.parse(body).Year);
+            console.log("IMDB Rating of the move is:" + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie is:" + JSON.parse(body).tomatoRating);
+            console.log("Country where the movie was produced is:" + JSON.parse(body).Country);
+            console.log("Language of the movie is:" + JSON.parse(body).Language);
+            console.log("Plot of the movie is:" + JSON.parse(body).Plot);
+            console.log("Actors in the movie are:" + JSON.parse(body).Actors);
+        }else{
+            console.log('Error occured.');
+        } if(movie === 'Mr. Nobody'){
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        }
+    });
+}
+/*
+// Command #4 LIRI will take the text inside of random.txt and use it to call one of LIRI's commands
+function doWhatItSays(){
+    fs.readFile('random.txt', "utf8", function(err, data){
+        if(err){
+            console.log('Error occurred.');
+        } else{
+            let content = data;
+            console.log(data); 
+            
+            spotifyThisSong();
+        }
+    })
+};
 
 
 
+*/
